528dfc7b270f2beed83a897f74ff230b3ff04ac8
Timmy <timmyliu1997@hotmail.com>
2023-09-19 14:51:47 +0800

    Initial Commit

---
+++ QA APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QA APP
+//
+//  Created by Timmy on 2023/9/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
